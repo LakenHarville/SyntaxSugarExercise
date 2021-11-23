9b9b8a5cc14819c2efd6c4577899074448d184fe
Laken Harville <rlakenharville@gmail.com>
2021-11-22 18:34:47 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var answer = 4;
+            string response;
+
+            response = (answer < 9) ? $"{answer} less than nine." : $"{answer} is greater than or equal to nine.";
+            Console.WriteLine(response);
+        }
+    }
+}
